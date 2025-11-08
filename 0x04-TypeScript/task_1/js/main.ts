d853b1023ad0ddf13eccdc8fb641025317a57455
Emmanuel Adi <adiemmanuel10@gmail.com>
2025-11-08 21:55:04 +0000

    Creating the directories task_1/js with typescript dependencies and main.ts file with 1. Building a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,34 @@
+
+ //  1. Building a Teacher interface
+interface Teacher {
+    readonly firstName: string, // modifiable only after initialized
+    readonly lastName: string, // modifiable only after initialized
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number, // optional
+    location: string,
+    [key: string]: any; // For the posibility to add extra attributes
+};
+
+// Example 1
+const teacher3: Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: false,
+    location: "London",
+    contract: false, // this extra attribute was not listed above (no typed error)
+
+    // NB: yearsOfExperience attribute was excluded but no typed error because it was set to optional (?)
+};
+
+// Example 2
+const teacher4: Teacher = {
+  firstName: "John",
+  lastName: "Agalga",
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: "Accra",
+  contract: false,
+  department: "Science" // another extra attribute
+};
+
+console.log(teacher3);
